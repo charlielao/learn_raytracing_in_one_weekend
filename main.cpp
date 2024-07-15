9eb751d33de5a664355b7c5825ba762164b2fd4f
charlielao <charlielao@Charlies-MacBook-Pro.local>
2024-07-15 21:24:58 +0100

    setup vec3 class

--- main.cpp
+++ main.cpp
@@ -2,18 +2,18 @@
 
 int main()
 {
-    //Image
+
+    // Image
 
     int image_width = 256;
     int image_height = 256;
 
-    //Render
+    // Render
+
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j=0; j<image_height; j++)
-    {
-        for (int i=0; i<image_width; i++)
-        {
+    for (int j = 0; j < image_height; j++) {
+        for (int i = 0; i < image_width; i++) {
             auto r = double(i) / (image_width-1);
             auto g = double(j) / (image_height-1);
             auto b = 0.0;
